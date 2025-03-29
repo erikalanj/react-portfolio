6a194128ddd5e1d5ff7e8e6ab7e586dda360bb0e
erikjensen <erikj@my.ccsu.edu>
2025-03-29 17:07:48 -0400

    hero section and header fully implemented

---
+++ portfolio-react/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from './components/navbar/navbar';
+import Hero from './components/hero/Hero';
+import About from './components/about/About';
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <About/>
+    </div>
+  );
+};
+
+export default App;
